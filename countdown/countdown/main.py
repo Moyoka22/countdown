919a922f8d971ea84520dc65785bbc3b1162c375
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2021-06-04 00:37:18 +0100

    Fixed master merge.

--- countdown/countdown/main.py
+++ countdown/countdown/main.py
@@ -4,9 +4,31 @@ from typing import Any, Dict
 from .Countdown import Countdown
 from .Listener import Listener
 from .parser import Parser
+from .utils import JoinListAction
 
 
-def main(args: Dict[str, Any]):
+def main():
+    args = parse_script_arguments()
+    countdown_main(vars(args))
+
+
+def parse_script_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('duration',
+                        action=JoinListAction,
+                        nargs='+',
+                        help='A string specifying the duration for which the '
+                        'timer should be run.')
+    parser.add_argument('--printer', '-p', default='curses', type=str,
+                        help='The printer to use for displaying the'
+                        'remaining time.'
+                        )
+    parser.add_argument('--alarm', '-a', default='silent', type=str,
+                        help='The alarm to ring when the timer is finished.')
+    return parser.parse_args()
+
+
+def countdown_main(args: Dict[str, Any]):
 
     countdown_args = Parser(args).get_arguments()
 
@@ -19,24 +41,5 @@ def main(args: Dict[str, Any]):
     countdown.join()
 
 
-def parse_script_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('duration', type=str,
-                        help='A string specifying the duration for which the '
-                        'timer should be run.')
-    parser.add_argument('--printer', '-p', default='curses', type=str,
-                        help='The printer to use for displaying the'
-                        'remaining time.'
-                        )
-    parser.add_argument('--alarm', '-a', default='silent', type=str,
-                        help='The alarm to ring when the timer is finished.')
-    return parser.parse_args()
-
-
-def entry():
-    args = parse_script_arguments()
-    main(vars(args))
-
-
 if __name__ == '__main__':
-    entry()
+    main()
